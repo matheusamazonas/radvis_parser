cedde33ce085a22a005901d7b284b1d42e67c224
Matheus Amazonas <matheus.amazonas@gmail.com>
2022-08-05 18:54:38 +0200

    ✅ Update example input file path

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,5 +1,5 @@
 ﻿using RADVisParser.Parsers.VTK;
 
-var parser = new VTKParser("/Users/matheus/Development/radvis-parser/Example/Inputs/track_yz.vtk");
+var parser = new VTKParser("/Users/matheus/Development/radvis_parser/Example/Inputs/track_yz.vtk");
 var file = parser.Parse();
 Console.WriteLine($"Header:{file.Header}. Dimensions: {file.Dimensions}. Positions: {file.Positions.Length}");
